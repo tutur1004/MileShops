c20781e9029dab00d2227a6273c9617a5e06ed0d
Arthur <arthur.barthelme78@gmail.com>
2023-07-26 01:20:26 +0200

    Clean ElasticSearch, now with try-with-resources
    - All requests are now in a try-with-resources block to prevent connection reset (Next optimisation: Add a money cache)
    - Due to try-with-resources usage, all async calls are removed
    - Simplify Elastic catches
    - Properly return true/false on get storage

--- src/main/java/fr/milekat/shops/storage/adapter/elasticsearch/ESConnection.java
+++ src/main/java/fr/milekat/shops/storage/adapter/elasticsearch/ESConnection.java
@@ -1,68 +1,26 @@
 package fr.milekat.shops.storage.adapter.elasticsearch;
 
-import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.milekat.shops.Main;
-import fr.milekat.utils.Configs;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-public class ESConnection {
+public class ESConnection implements AutoCloseable {
     private final RestClient restClient;
-    private final ElasticsearchTransport transport;
-    public final JacksonJsonpMapper jsonpMapper;
+    private final RestClientTransport transport;
 
-    public ESConnection(@NotNull Configs config) {
-        //  Fetch connections vars from config.yml file
-        String hostname = config.getString("storage.elasticsearch.hostname");
-        int port = config.getInt("storage.elasticsearch.port", 9200);
-        String username = config.getString("storage.elasticsearch.username", "null");
-        String password = config.getString("storage.elasticsearch.password", "null");
-        //  Check hostname/port
-        Main.debug("Hostname:" + hostname);
-        Main.debug("Port:" + port);
-        Main.debug("Username:" + username);
-        //  Init the RestClientBuilder
-        RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost(hostname, port));
-        //  If credentials are set, apply credentials to RestClientBuilder
-        if (!username.equals("null") && !password.equals("null")) {
-            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
-            restClientBuilder.setHttpClientConfigCallback(httpClientBuilder ->
-                    httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
-        }
-
-        //  Load RestClientTransport
-        restClient = restClientBuilder.build();
+    public ESConnection(RestClient restClient) {
+        this.restClient = restClient;
         //  Load custom mapper(s) [Optional]
-        jsonpMapper = new JacksonJsonpMapper();
+        JacksonJsonpMapper jsonpMapper = new JacksonJsonpMapper();
         jsonpMapper.objectMapper().registerModule(new CustomMappers().getModule());
-        //  Build ElasticsearchTransport
-        transport = new RestClientTransport(restClient, jsonpMapper);
-    }
-
-    public ObjectMapper getMapper() {
-        return jsonpMapper.objectMapper();
+        this.transport = new RestClientTransport(restClient, jsonpMapper);
     }
 
     public ElasticsearchClient getClient() {
         return new ElasticsearchClient(transport);
-    }
-
-    public ElasticsearchAsyncClient getAsyncClient() {
-        return new ElasticsearchAsyncClient(transport);
     }
 
     public void close() throws IOException {
