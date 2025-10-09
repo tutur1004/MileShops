2cd1c47a1ca8764c16e3b77a3119a4654c5dc0f2
Arthur <arthur.barthelme78@gmail.com>
2025-10-10 01:12:05 +0200

    Refactor HeadsUtils to use PaperMC PlayerProfile for skull textures and update build.gradle for PaperMC dependency

--- src/main/java/fr/milekat/shops/workers/utils/HeadsUtils.java
+++ src/main/java/fr/milekat/shops/workers/utils/HeadsUtils.java
@@ -1,13 +1,13 @@
 package fr.milekat.shops.workers.utils;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import fr.milekat.shops.Main;
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Field;
 import java.util.UUID;
 
 public enum HeadsUtils {
@@ -27,26 +27,17 @@ public enum HeadsUtils {
      * @param meta   the skull meta to modify
      * @param base64 the base64 encoded string
      */
-    private static void setSkinViaBase64(SkullMeta meta, String base64) {
-        Field profileField;
-        try {
-            GameProfile profile = new GameProfile(UUID.randomUUID(), "skull-texture");
-            profile.getProperties().put("textures", new Property("textures", base64));
-            profileField = meta.getClass().getDeclaredField("profile");
-            profileField.setAccessible(true);
-            profileField.set(meta, profile);
-        } catch (IllegalAccessException | NoSuchFieldException exception) {
-            Main.getMileLogger().warning("There was a severe internal reflection " +
-                    "error when attempting to set the skin of a player skull via base64!");
-            Main.getMileLogger().stack(exception.getStackTrace());
-        }
+    private static void setSkinViaBase64(@NotNull SkullMeta meta, String base64) {
+        PlayerProfile profile = Bukkit.createProfile(UUID.randomUUID(), null);
+        profile.setProperty(new ProfileProperty("textures", base64));
+        meta.setPlayerProfile(profile);
     }
 
     public String getTexture() {
         return texture;
     }
 
-    public ItemStack getItem() {
+    public @NotNull ItemStack getItem() {
         ItemStack itemStack = new ItemStack(Material.PLAYER_HEAD, 1);
         SkullMeta skullMeta = (SkullMeta) itemStack.getItemMeta();
 
